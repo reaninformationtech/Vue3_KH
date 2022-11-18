00236d0f1f8498b901a759c93f69e403404897a6
JoinCoder Journey <82702646+joincoders@users.noreply.github.com>
2022-11-18 15:50:19 +0700

    Vue 3 v-on event
    We can use the v-on directive, which we typically shorten to the @ symbol, to listen to DOM events and run some JavaScript when they're triggered. The usage would be v-on:click="handler" or with the shortcut, @click="handler".

---
+++ Vue3 vent increase & decrease or change title/app.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+    data(){
+        return {
+            title: 'Rean-iT for Cambodia',
+            year : '2025'
+        }
+    },
+    methods:{
+        changenewtitle(){
+            this.title='រៀនអាយធី Rean-iT'
+        }
+    }
+}).mount('#app')
